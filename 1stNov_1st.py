f89fe65d44c34bb64e341318cbd317ee5b73bbf5
AlexShim99 <116084590+AlexShim99@users.noreply.github.com>
2022-11-21 18:50:41 -0800

    Delete 1stNov_1st.py

--- 1stNov_1st.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-age = int(input('Enter your age in years: '))
-max_hear_rate = 206.9 - (0.67 * age)
-target = 0.65 * max_hear_rate
-print ("Your target fat-burning heart", \
-"rate is", target)
-
